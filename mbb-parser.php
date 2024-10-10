a0d4c959e789b5d6322041ddb8127dc341c107a8
LyNguyen <cnttnt@gmail.com>
2024-10-10 16:41:35 +0700

    Fix: plugin Check

--- mbb-parser.php
+++ mbb-parser.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: MBB Parser
- * Plugin URI:  https: //metabox.io
+ * Plugin URI:  https://metabox.io
  * Description: Parses meta box settings from JSON.
  * Version:     2.0.0
  * Author:      MetaBox.io
- * Author URI:  https: //metabox.io
+ * Author URI:  https://metabox.io
  */
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
