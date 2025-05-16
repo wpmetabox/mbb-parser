a204be2292cbf88545d3939f13007e4c27c4b032
Anh Tran <anhtnt@elightup.com>
2025-05-16 16:56:14 +0700

    Parse submitted field json string to array

---
+++ src/Parsers/FieldJson.php
@@ -0,0 +1,25 @@
+<?php
+namespace MBBParser\Parsers;
+
+class FieldJson extends Base {
+	public function parse(): void {
+		$this->parse_fields( $this->settings );
+	}
+
+	private function parse_fields( &$fields ): void {
+		array_walk( $fields, [ $this, 'parse_field' ] );
+	}
+
+	private function parse_field( &$field ): void {
+		if ( ! is_string( $field ) ) {
+			return;
+		}
+
+		$json = json_decode( $field, true );
+		if ( ! is_array( $json ) || json_last_error() !== JSON_ERROR_NONE ) {
+			return;
+		}
+
+		$field = $json;
+	}
+}
